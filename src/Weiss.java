c236cfdff110e2b42854d2dff10de424d7369a83
AdamMYoung <me@adam-young.xyz>
2017-01-02 20:14:26 +0000

    Added new GUI

--- src/Weiss.java
+++ src/Weiss.java
@@ -1,8 +1,8 @@
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import weiss.agent.NodeMonitor;
-import weiss.message.UserMessage;
+import WeissManager.WeissManager;
+import java.util.logging.*;
+import weiss.agent.*;
+import weiss.message.*;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -22,16 +22,46 @@ public class Weiss {
      */
     public static void main(String[] args) {
         
+        WeissManager manager = new WeissManager();
         
+        
+        MetaAgent portal = new Portal("P1", null);
+        portal.start();
+        
+        MetaAgent agent1 = new Agent("Fred", portal);
+        agent1.start();
+        MetaAgent agent2 = new Agent("Phill", portal);
+        agent2.start();
+        MetaAgent agent3 = new Agent("Phill", portal);
+        
+        try
+        {
+            portal.put(new SysMessage("Admin", "P1", "reg", agent1));
+            portal.put(new SysMessage("Admin", "P1", "reg", agent2));
+            portal.put(new SysMessage("Admin", "P1", "reg", agent3));
+        } 
+        catch (InterruptedException ex)
+        {
+            Logger.getLogger(Weiss.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        agent1.sendMessage("Phill", "Hello World!");
+        agent1.sendMessage("Greg", "Hello World!");
+        
+        
+        
+        /*     
         NodeMonitor n = new NodeMonitor();
         n.start();
         try
         {
             n.put(new UserMessage("Fred", "You", "Hello"));
-        } catch (InterruptedException ex)
+        }
+        catch (InterruptedException ex)
         {
             Logger.getLogger(Weiss.class.getName()).log(Level.SEVERE, null, ex);
         }
+`       */
     }
 
 }
