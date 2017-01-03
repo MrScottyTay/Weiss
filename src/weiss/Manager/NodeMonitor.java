79e535eb85a865a7b6d9ce7063b48bde4d94d753
AdamMYoung <me@adam-young.xyz>
2017-01-03 00:08:52 +0000

    Added Licencing Heading

--- src/weiss/Manager/NodeMonitor.java
+++ src/weiss/Manager/NodeMonitor.java
@@ -1,7 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2017 Adam Young
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package Weiss.Manager;
 
@@ -10,19 +21,17 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.util.Vector;
 import javax.swing.JDialog;
-import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import weiss.MetaAgent.MetaAgent;
 import weiss.MetaAgent.WeissBase;
-import weiss.message.Message;
 import weiss.message.SysMessage;
 import weiss.message.UserMessage;
 
 /**
  *
- * @author Adam Young
+ * @author Adam Young, Teesside University Sch. of Computing
  */
 public class NodeMonitor extends WeissBase implements Runnable
 {
